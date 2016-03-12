4538f673e7b83397026e05ccc87300246bbf5cee
Nicolas Brignol <tools@octet-studio.com>
2016-03-12 10:45:50 +0100

    Added form add scene (super basic)

--- src/AppBundle/Controller/SceneFormController.php
+++ src/AppBundle/Controller/SceneFormController.php
@@ -26,7 +26,7 @@ class SceneFormController extends Controller
 
 		$scene = new Scene();
 
-        $formSceneBuilder = $this->createFormBuilder($scene);
+        $formSceneBuilder = $this->createFormBuilder();
 
 		$formSceneBuilder->add('project', EntityType::class, array(
     		'class' => 'AppBundle:Writer\Project',
@@ -37,11 +37,70 @@ class SceneFormController extends Controller
 
 		$formSceneBuilder
 			->add('title', TextType::class)
+            ->add('text', TextType::class)
             ->add('save', SubmitType::class, array('label' => 'Create Scene'));
 
-    
+        $form = $formSceneBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project = $form["project"]->getData();
+            $title = $form["title"]->getData();
+            $text = $form["text"]->getData();
+
+            $scene = $this->makeScene($title, $project);
+            $this->makeMediaText($text, $scene);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            $this->addFlash("notice", "Saved scene : " . $scene->getId());
+            return $this->redirectToRoute('previewProject', array('sceneId'=> $scene->getId()));
+        }
+
         return $this->render('writer/sceneForm.html.twig', array(
-            'formScene' => $formSceneBuilder->getForm()->createView()
+            'formScene' => $form->createView()
         ));
+    }
+
+    protected function makeMediaText($content, $inScene=null) {
+        $media = new Media();
+        $media->setFormat("text");
+        $media->setContent($content);
+
+        if ($inScene) {
+            $media->setScene($inScene);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($media);
+
+        return $media;
+    }
+
+    protected function makeConnection($scene1, $scene2, $label="", $pattern=""){
+        $connection = new SceneConnection();
+        $connection->setParentScene($scene1);
+        $connection->setChildScene($scene2);
+
+        $connection->setLabel($label);
+        $connection->setPattern($pattern);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($connection);
+
+        return $connection;
+    }
+
+    protected function makeScene($title, $project){
+        $scene = new Scene();
+        $scene->setTitle($title);
+        $scene->setProject($project);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($scene);
+
+        return $scene;
     }
 }
