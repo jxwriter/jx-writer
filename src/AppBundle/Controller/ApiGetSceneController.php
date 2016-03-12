4d46c00f33555322905bb18064654cb23abad6b7
Nicolas Brignol <tools@octet-studio.com>
2016-03-12 10:35:37 +0100

    Api getScene basic JSON

--- src/AppBundle/Controller/ApiGetSceneController.php
+++ src/AppBundle/Controller/ApiGetSceneController.php
@@ -6,13 +6,55 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class ApiGetSceneController extends Controller
 {
     /**
-     * @Route("/api/scene/{id}", name="apiGetScene")
+     * @Route("/api/scene/{sceneId}", name="apiGetScene")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $sceneId)
     {
-        return new JSONResponse(array());
+    	$sceneRepo = $this->getDoctrine()->getRepository('AppBundle:Writer\Scene');
+        
+        $scene = $sceneRepo->find($sceneId);
+        $project = $scene->getProject();
+
+        $result = array(
+        	"id" => $scene->getId(),
+        	"title" => $scene->getId(),
+        	"actions" => $scene->getActions(),
+        	"conditions" => $scene->getConditions(),
+        	"medias" => array(),
+        	"connections" => array(),
+        );
+
+        $medias = $scene->getMedias();
+
+        foreach ($medias as $media) {
+        	$m = array(
+        		"format" => $media->getFormat(),
+        		"content" => $media->getContent(),
+        		"conditions" => $media->getConditions(),
+        	);
+
+        	$result["medias"][] = $m;
+        }
+
+        $connections = $scene->getConnections();
+
+        foreach ($connections as $connection) {
+        	$c = array(
+        		"label" => $connection->getLabel(),
+        		"pattern" => $connection->getPattern(),
+        		"conditions" => $connection->getConditions(),
+        		"childSceneId" => $connection->getChildScene()->getId(),
+        	);
+
+        	$result["connections"][] = $c;
+        }
+
+
+        return new JSONResponse($result);
     }
 }
