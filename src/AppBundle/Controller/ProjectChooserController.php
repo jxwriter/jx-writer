9879da10dd605217484a134d8b80641bf6610264
Nicolas Brignol <tools@octet-studio.com>
2016-03-15 16:04:23 +0100

    Non working Project chooser

--- src/AppBundle/Controller/ProjectChooserController.php
+++ src/AppBundle/Controller/ProjectChooserController.php
@@ -13,7 +13,15 @@ class ProjectChooserController extends Controller
      */
     public function indexAction(Request $request)
     {
+		$projectRepo = $this->getDoctrine()->getRepository('AppBundle:Writer\Project');
 
-        return $this->render('writer/projectChooser.html.twig');
+		if ($request->query->has("projectId")) {
+			//add to session
+			//redirect to list
+		}
+
+    	$projects = $projectRepo->findAll();
+        return $this->render('writer/projectChooser.html.twig', 
+        	array("projects"=>$projects));
     }
 }
